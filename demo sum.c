9ff546f94bad231a9b502e941b73c97ce17715c1
Siddharth Kumar <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-27 20:23:55 +0530

    demo code of C language

---
+++ demo sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a,b;//c;
+	printf("Enter the value of A: ");
+	scanf("%d",&a);
+	printf("Enter the valuue of B: ");
+	scanf("%d",&b);
+	//c= a+b;
+	//c=a-b;
+	//c=a*b;
+	//c=a/b;
+	printf("Add= %d\nsub= %d\nmul= %d\ndiv= %d\n",a+b,a-b,a*b,a/b);
+}
