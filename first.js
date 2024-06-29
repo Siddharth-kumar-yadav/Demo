a65aaef6467a14c849b2fda1ffa3bda6d03a54cb
Siddharth Kumar <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-29 23:58:04 +0530

    my first java script code

---
+++ first.js
@@ -0,0 +1,28 @@
+Name = "Siddharth kumar yadav"
+var age = "22";
+console.log(Name);
+console.log(age);
+age + 1;
+x =null;
+y= undefined;
+console.log(x);
+isfollow =true;
+console.log(isfollow);
+isntfollow=false;
+console.log(isntfollow);
+{
+    let a=5;
+    console.log(a);
+}
+{
+    let a =10;
+    console.log(a);
+}
+//object.
+const student={
+    Name: "Siddharth Kumar",
+    Class: "Bca",
+    cgpa: "8.5"
+};
+console.log(age + "1");
+console.log("Hello worls");
