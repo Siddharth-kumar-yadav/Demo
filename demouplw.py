588b2a9eabb7b2637e3f88c9891ac79a7c36aeaa
Siddharth Kumar <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-07-13 19:40:03 +0530

    demo code of python

---
+++ demouplw.py
@@ -0,0 +1,12 @@
+print(' String function')
+name = "Akash Kumar"
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.find('m'))
+print(name.replace('Akash','Abhimanyu'))
+print(name)
+nam = "Siddharth"
+print(nam.find('r'))
+print('k' in name)
+print('k' in nam)
